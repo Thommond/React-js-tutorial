da6280dc87f425239dc04cd66d9edbdae873ea09
Thom Mondeaux <Thommond554@gmail.com>
2020-07-22 15:18:58 -0400

    Added handleClick to Board class, for handling clicks.
    Added a function to check if there is a winner to the game.
    
    Updated Square class to a function component.

--- src/index.js
+++ src/index.js
@@ -2,31 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+let Square = (props) => {
+  return (
+    <button className="square"  onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      value: null
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
-  render() {
-    return (
-      <button className="square" onClick={ () => { alert('click'); }}>
-        {this.props.value}
-      </button>
-    );
+  handleClick(i) {
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
   }
+
+  renderSquare(i) {
+    return (
+      <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
+  );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
   render() {
-    const status = 'Next player: X';
+
+    const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -53,6 +70,14 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' :
+      'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
@@ -65,6 +90,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+let calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; 9 < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a]
+      === squares[c]) {
+        return squares[a];
+      }
+  }
+  return null;
 }
 
 // ========================================
